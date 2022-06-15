37558bbe6b6a464635deceb0a6d07a36f5e2bb58
reeditor <roman.tymoshchuk@oa.edu.ua>
2022-06-15 20:56:07 +0300

    Added client interaction with API

---
+++ source/Rewinery.Client.Infractructure/HttpCategoriesRepository.cs
@@ -0,0 +1,15 @@
+﻿using Rewinery.Shared.Dtos.WinesDtos;
+using System.Net.Http.Json;
+
+namespace Rewinery.Client.Infrastructure
+{
+    public class HttpCategoriesRepository : HttpBaseRepository
+    {
+        public HttpCategoriesRepository(HttpClient httpClient) : base(httpClient) { }
+
+        public async Task<IEnumerable<WineReadDto>> GetAllAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<IEnumerable<WineReadDto>>($"/api/wines");
+        }
+    }
+}
